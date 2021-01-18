8514fd0b6af518c0843c611fc37da3643310fd8c
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2021-01-18 09:46:57 -0400

    Initial Commit

---
+++ All-Indie-SwiftUITests/All_Indie_SwiftUITests.swift
@@ -0,0 +1,33 @@
+//
+//  All_Indie_SwiftUITests.swift
+//  All-Indie-SwiftUITests
+//
+//  Created by Rebeca Pacheco on 18/01/21.
+//
+
+import XCTest
+@testable import All_Indie_SwiftUI
+
+class All_Indie_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
