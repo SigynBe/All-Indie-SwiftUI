a194689e6a79823f8f1519f19f22c8e8e99e7d4c
Alessandro Negrão <alessandro.negrao.s@gmail.com>
2021-01-22 13:29:36 -0400

    Added a new context custom view and removed spacing between image and it

--- All-Indie-SwiftUI/View/ContentView.swift
+++ All-Indie-SwiftUI/View/ContentView.swift
@@ -9,26 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: true){
-                HStack{
-                    VStack(alignment: .leading){
-                        Image("hoje")
+        ZStack{
+            Color(red: 1.00, green: 1.00, blue: 0.96, opacity: 1.00)
+                .ignoresSafeArea()
+            VStack(){
+                ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: true){
+                    HStack{
+                        VStack(alignment: .leading){
+                            Image("hoje")
+                        }
+                        Spacer()
+                        Button(action: {print("button tapped")}) {
+                            Image(systemName: "bell.fill").font(.system(size: 30)).foregroundColor(.black)
+                        }
+                    }.padding()
+                    VStack(spacing: 0){
+                        Image("capa")
+                            .resizable()
+                            .frame(height: 485, alignment: .center)
+                            .padding(0)
+                        CustomView().padding(0)
                     }
-                    
-                    Spacer()
-                    Button(action: {print("button tapped")}) {
-                        Image(systemName: "bell.fill").font(.system(size: 30)).foregroundColor(.black)
-                        
-                    }
-                    
-                }.padding()
-                Image("capa").resizable().frame(height: 600, alignment: .center)
-                Image("capa").resizable().frame(height: 600, alignment: .center)
-                Image("capa").resizable().frame(height: 600, alignment: .center)
-                Image("capa").resizable().frame(height: 600, alignment: .center)
-                Image("capa").resizable().frame(height: 600, alignment: .center)
-                Image("capa").resizable().frame(height: 600, alignment: .center)
+                   
+                }.padding(.bottom)
+                .padding(.leading)
+                .padding(.trailing)
             }
         }
     }
