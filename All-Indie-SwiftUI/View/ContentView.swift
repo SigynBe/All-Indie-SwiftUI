afa9147a60d13a142f6fa2a23beccf88809da857
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2021-01-25 09:40:28 -0400

    tem uma collection view

--- All-Indie-SwiftUI/View/ContentView.swift
+++ All-Indie-SwiftUI/View/ContentView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let data: [DataModel] = [
+        .init(id: "0", imageName: "capa1"),.init(id: "1", imageName: "capa2"),.init(id: "2", imageName: "capa3"),.init(id: "3", imageName: "capa4"),
+    ]
+    
     
     var body: some View {
+        
         ZStack{
             Color(red: 1.00, green: 1.00, blue: 0.96, opacity: 1.00)
                 .ignoresSafeArea()
@@ -25,20 +30,34 @@ struct ContentView: View {
                         }
                     }.padding()
                     VStack(spacing: 0){
-                        Image("capa")
+                        Image("capa1")
                             .resizable()
                             .frame(height: 485, alignment: .center)
                             .padding(0)
                         CustomView().padding(0)
                     }
-                   
+                    
+                    
+                    List {
+                        ForEach (data) {items in
+                            ForEach (0 ..< 2) {item in
+                                CollectionViewCell (data: items)
+                            }
+                        }
+                    }                    
+                    
+                    
+                    
                 }.padding(.bottom)
                 .padding(.leading)
                 .padding(.trailing)
             }
             
+            
         }
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
