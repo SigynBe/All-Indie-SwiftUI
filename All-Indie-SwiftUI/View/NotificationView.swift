2d1e96ed2ccc7849bf541433bc927e5339afc720
Alessandro Negrão <alessandro.negrao.s@gmail.com>
2021-01-26 10:32:03 -0400

    Created NotificationView

---
+++ All-Indie-SwiftUI/View/NotificationView.swift
@@ -0,0 +1,30 @@
+//
+//  NotificationView.swift
+//  All-Indie-SwiftUI
+//
+//  Created by Alessandro Negrão on 26/01/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        ZStack{
+            Color(red: 1.00, green: 0.99, blue: 0.91, opacity: 1.00)
+                .ignoresSafeArea()
+            VStack{
+                HStack{
+                    
+                }
+                //Text()
+                //PickerView
+            }
+        }
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView().previewLayout(.sizeThatFits)
+    }
+}
